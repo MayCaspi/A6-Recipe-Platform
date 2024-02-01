902b8afd341489130a51c3d46068fff440468052
Matan <cz.matan2@gmail.com>
2024-02-01 18:38:07 +0200

    dark mode implementation

---
+++ main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded",function()
+{
+    document.getElementById("btnTheme").addEventListener("click",function()
+    {
+        document.documentElement.classList.toggle("dark");
+        document.getElementById("btnTheme").innerText = document.documentElement
+    .classList.contains("dark")?"light":"dark"
+    })
+})
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   content: ["./*.{html,js,ts}"],
-  darkMode:false,
+  darkMode:'class',
   theme: {
     extend: {},
   },
