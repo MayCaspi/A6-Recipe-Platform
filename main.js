ac3ad83f806861d926ca45bee875fc65b753ac54
tzahbak <tzahi.bakal@gmail.com>
2024-02-02 12:49:50 +0200

    Added name box in adding recipe section, highlightMenu func in JS and highlight class in css

--- main.js
+++ main.js
@@ -1,13 +1,39 @@
-document.addEventListener("DOMContentLoaded",function()
-{
-    document.getElementById("btnTheme").addEventListener("click",function()
-    {
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("btnTheme").addEventListener("click", function () {
         document.documentElement.classList.toggle("dark");
         document.getElementById("btnTheme").innerText = document.documentElement
-    .classList.contains("dark")?"light":"dark"
+            .classList.contains("dark") ? "light" : "dark"
     })
-    document.getElementById("btnMenu").addEventListener("click",function()
-    {
+    document.getElementById("btnMenu").addEventListener("click", function () {
         document.getElementById("popUp").classList.toggle("hidden")
     })
 })
+
+// Show active menu when scrolling
+const highlightMenu = () => {
+    const elem = document.querySelector('.highlight');
+    const homeMenu = document.querySelector('#home-page');
+    const newRecipeMenu = document.querySelector('#newRecipe-page');
+    const mealPlannerMenu = document.querySelector('#mealPlanner-page');
+    let scrollPos = window.scrollY;
+    // console.log(scrollPos); 
+    // used to find the correct values for scrollPos (try to run it on your browser's console)
+
+    // adds 'highlight' class to menu items
+    if (scrollPos < 750) {
+        homeMenu.classList.add('highlight');
+        newRecipeMenu.classList.remove('highlight');
+        return;
+    } else if (scrollPos < 1700) {
+        newRecipeMenu.classList.add('highlight');
+        homeMenu.classList.remove('highlight');
+        mealPlannerMenu.classList.remove('highlight');
+        return;
+    } else if (scrollPos < 2345) {
+        mealPlannerMenu.classList.add('highlight');
+        newRecipeMenu.classList.remove('highlight');
+        return;
+    }
+};
+
+window.addEventListener('scroll', highlightMenu);
