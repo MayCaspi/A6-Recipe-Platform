d7b759c76503e065cd3d4958637b92b7956108f8
Matan <cz.matan2@gmail.com>
2024-02-23 18:07:47 +0200

    users creator

---
+++ LoginFolder/usersCreator.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+
+// New users to add
+const newUsers = [
+  { username: 'amir', password: '12345678' },
+  { username: 'ron', password: '12345678' },
+  { username: 'avi', password: '12345678' },
+  { username: 'adi', password: '12345678' },
+  { username: 'matan', password: '12345678' },
+  { username: 'daniel', password: '12345678' },
+  { username: 'tzahi', password: '12345678' }
+];
+
+// Encode username and password for new users
+newUsers.forEach(user => {
+  user.username = Buffer.from(user.username).toString('base64');
+  user.password = Buffer.from(user.password).toString('base64');
+});
+
+
+// Read existing users from the users.json file
+let existingUsers = [];
+try {
+  existingUsers = JSON.parse(fs.readFileSync('users.json', 'utf-8'));
+  // Combine existing and new users
+} catch (error) {
+  console.error('Error reading existing users:', error);
+}
+
+const allUsers = existingUsers.concat(newUsers);
+try{
+// Write the updated array of users back to the users.json file
+fs.writeFileSync('users.json', JSON.stringify(allUsers, null, 2));
+console.log('New users added successfully.');
+}catch(error){
+    console.error('Error writin new users:', error);  
+}
+
+
